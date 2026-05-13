0749b4261aa10814e8c90ff21aafb2e5807c0fcb
YassinOs-dot <yassineabdelwahed11@gmail.com>
2026-05-13 13:21:43 +0100

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -1,25 +1,51 @@
+#!/usr/bin/env python
+"""Train PPO agent on the welding environment."""
 import yaml
+import argparse
+import os
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize
+from stable_baselines3.common.monitor import Monitor
 from drl_welding.envs.welding_env import WeldingEnv
 
 def make_env(config, rank):
     def _init():
         env = WeldingEnv(config)
+        env = Monitor(env)
         return env
     return _init
 
-if __name__ == "__main__":
-    with open("config/default.yaml") as f:
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='config/default.yaml', help='Path to config file')
+    args = parser.parse_args()
+
+    with open(args.config) as f:
         config = yaml.safe_load(f)
-    
-    num_envs = 4
-    env = SubprocVecEnv([make_env(config, i) for i in range(num_envs)])
-    
-    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/",
-                n_steps=2048, batch_size=64, learning_rate=3e-4,
-                gamma=0.99, gae_lambda=0.95, ent_coef=0.01,
-                policy_kwargs=dict(net_arch=[256,256], activation_fn='tanh'))
-    
-    model.learn(total_timesteps=2_000_000)
-    model.save("ppo_welder")
+
+    n_envs = config.get('n_envs', 4)
+    env = SubprocVecEnv([make_env(config, i) for i in range(n_envs)])
+    # Normalise observations and rewards (optional)
+    # env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
+
+    model = PPO(
+        "MlpPolicy",
+        env,
+        verbose=1,
+        tensorboard_log="./logs/",
+        n_steps=config['n_steps'],
+        batch_size=config['batch_size'],
+        learning_rate=config['learning_rate'],
+        gamma=config['gamma'],
+        gae_lambda=config['gae_lambda'],
+        ent_coef=config['ent_coef'],
+        policy_kwargs={'net_arch': config['net_arch']}
+    )
+
+    model.learn(total_timesteps=config['total_timesteps'])
+    os.makedirs('models', exist_ok=True)
+    model.save("models/ppo_welder")
+    print("Training finished, model saved.")
+
+if __name__ == "__main__":
+    main()
